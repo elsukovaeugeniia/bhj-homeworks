a624bbff7257b50891e92a73b0a2ef70b6c27e31
Elsukova Eugeniia <elsukova.eugeniia@gmail.com>
2025-12-09 20:33:52 +0300

    Соло клавиатура

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,23 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keyup', (event) => {
+    
+    const inputChar = event.key.toLowerCase();
+    
+    
+    if (!this.currentSymbol) return;
+    
+    
+    const currentChar = this.currentSymbol.textContent.toLowerCase();
+    
+    
+    if (inputChar === currentChar) {
+      this.success();
+    } else {
+      this.fail();
+    }
+  });
   }
 
   success() {
